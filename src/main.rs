1ed1c80636b7c5c089b012bb5f052a53061c76b9
Ben Weinstein-Raun <b@w-r.me>
2022-12-29 03:28:26 -0800

    begin adding git graph traversal stuff

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,49 @@ use notify_debouncer_mini::{DebounceEventResult, DebouncedEvent, new_debouncer, 
 
 fn try_commit(r: &Repository) {
     // 1. What branch are we on?
-    let head_name = if let Ok(head_ref) = r.head() {
-        if let Some(target) = head_ref.symbolic_target() {
-            target.to_string()
+    let (headbranchname, headcommit) = if let Ok(head_ref) = r.head() {
+        if !head_ref.is_branch() {
+            eprintln!("Either the repository is empty, or is a bare repository, or is in a detached-head state.");
+            eprintln!("gwipt requires that you check out a non-empty branch.");
+            return
+        }
+        if let (Some(target), Ok(commit)) = (head_ref.shorthand(), head_ref.peel_to_commit()) {
+            (target.to_string(), commit)
         } else {
-            eprintln!("Either repository is empty, or is a bare repository, or is in a detached-head state.");
-            eprintln!("gwipt requires that you check out a non-empty branch in a non-bare repository.");
+            eprintln!("Either the repository is empty, or is a bare repository, or is in a detached-head state.");
+            eprintln!("gwipt requires that you check out a non-empty branch.");
             return
         }
     } else {
-        eprintln!("Either repository is empty, or is a bare repository, or is in a detached-head state.");
-        eprintln!("gwipt requires that you check out a non-empty branch in a non-bare repository.");
+        eprintln!("Either the repository is empty, or is a bare repository, or is in a detached-head state.");
+        eprintln!("gwipt requires that you check out a non-empty branch.");
         return
     };
+
+    let wipbranchname = String::from("wip/") + &headbranchname;
+    let headoid = headcommit.id();
+
+    // Does the wip branch already exist? If so, is HEAD reachable from it? If not, create it and
+    // point it at HEAD. otherwise, leave it be.
+
+    // Make the wip branch if it doesn't exist
+    let (wipbranch, wipcommit) = if let Ok(branch) = r.find_branch(&wipbranchname, git2::BranchType::Local) {
+        if let Ok(commit) = branch.get().peel_to_commit() {
+            (branch, commit)
+        } else {
+            (r.branch(&wipbranchname, &headcommit, true).unwrap(), headcommit)
+        }
+    } else {
+        (r.branch(&wipbranchname, &headcommit, true).unwrap(), headcommit)
+    };
+
+    let wipoid = wipcommit.id();
+
+    if let Ok(true) = (r.graph_descendant_of(wipoid, headoid).map(|desc| desc || (headoid == wipoid))) {
+        eprintln!("Is an ancestor");
+    } else {
+        eprintln!("Not an ancestor");
+    }
 
     // 2. Is there a diff?
     eprintln!("");
