6af51eb310e127f3f5772161c88e839a48e8e645
Ben Weinstein-Raun <b@w-r.me>
2023-01-07 07:56:17 -0800

    update license text at the top of main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 //
 // Unless required by applicable law or agreed to in writing, this software is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under the License.
+// Licenses for the specific language governing permissions and limitations under the Licenses.
 use backoff::{retry, ExponentialBackoff};
 use clap::Parser;
 use git2::Repository;
